2c4c32b8b43c89e6cbb649a48c3a2a849fcb39c4
Aviv <komemiav@gmail.com>
2018-03-07 17:59:11 +0200

    basic scripts

---
+++ Assets/Scripts/Base/Actor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Health : MonoBehaviour {
+
+    protected float health;
+    protected float damage;
+
+    public abstract void InflictDamage();
+}
